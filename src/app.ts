434fe70441634e984e3503e2aeb6246ffad0c49b
Gustavo <joseluisrodri540@gmail.com>
2025-04-26 22:16:08 -0400

    4to commit

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express, { Request, Response } from 'express';
 import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configurar archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Configurar motor de vistas EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 
 // Rutas
 app.get('/', (req: Request, res: Response) => {
