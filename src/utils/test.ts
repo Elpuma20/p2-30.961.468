f3e1c20eab449d1ae8b31f2bb8a24f9d1a096df3
Jose <joseluisrodri540@gmail.com>
2025-06-15 12:14:06 -0400

    Revert "V5.6"
    This reverts commit c27a351cefed7420017f5fc071c086f6c446f7e2.

--- src/utils/test.ts
+++
@@ -1,8 +0,0 @@
-import { getCountryFromIp } from "./geolocationService";
-
-const testIps = ["8.8.8.8", "1.1.1.1", "invalid_ip"];
-
-testIps.forEach(async (ip) => {
-  const country = await getCountryFromIp(ip);
-  console.log(`IP: ${ip} - País: ${country}`);
-});
